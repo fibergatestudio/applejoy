287a642acc3c0c019c99be53ef755156971864da
filimonenkovyacheslav <filimonenkovyacheslav@gmail.com>
2021-07-29 13:16:03 +0300

    Init commit

---
+++ catalog/controller/extension/module/featured.php
@@ -0,0 +1,96 @@
+<?php
+class ControllerExtensionModuleFeatured extends Controller {
+	public function index($setting) {
+		$this->load->language('extension/module/featured');
+
+		$this->load->model('catalog/product');
+
+		$this->load->model('tool/image');
+		$data["module_name"] = $setting["name"];
+
+		$data['products'] = array();
+
+		if (!$setting['limit']) {
+			$setting['limit'] = 4;
+		}
+
+		if (!empty($setting['product'])) {
+			$products = array_slice($setting['product'], 0, (int)$setting['limit']);
+
+			foreach ($products as $product_id) {
+				$product_info = $this->model_catalog_product->getProduct($product_id);
+
+				if ($product_info) {
+					if ($product_info['image']) {
+						$image = $this->model_tool_image->resize($product_info['image'], $setting['width'], $setting['height']);
+					} else {
+						$image = $this->model_tool_image->resize('placeholder.png', $setting['width'], $setting['height']);
+					}
+
+					if ($this->customer->isLogged() || !$this->config->get('config_customer_price')) {
+						$price = $this->currency->format($this->tax->calculate($product_info['price'], $product_info['tax_class_id'], $this->config->get('config_tax')), $this->session->data['currency']);
+					} else {
+						$price = false;
+					}
+
+					if (!is_null($product_info['special']) && (float)$product_info['special'] >= 0) {
+						$special = $this->currency->format($this->tax->calculate($product_info['special'], $product_info['tax_class_id'], $this->config->get('config_tax')), $this->session->data['currency']);
+						$tax_price = (float)$product_info['special'];
+					} else {
+						$special = false;
+						$tax_price = (float)$product_info['price'];
+					}
+
+					if ($this->config->get('config_tax')) {
+						$tax = $this->currency->format($tax_price, $this->session->data['currency']);
+					} else {
+						$tax = false;
+					}
+
+					if ($this->config->get('config_review_status')) {
+						$rating = $product_info['rating'];
+					} else {
+						$rating = false;
+					}
+					$categories = $this->model_catalog_product->getCategories($product_info['product_id']);
+					$category_id = $categories[0]["category_id"];
+					$parent_id = $this->model_catalog_product->get_parent_id((int)$category_id);
+          while((int)$parent_id != 0){
+						$parent_id = $this->model_catalog_product->get_parent_id((int)$category_id);
+						if((int)$parent_id != 0){
+							$category_id = $parent_id;
+						}
+					}
+
+
+
+
+$this->load->model('catalog/tree_cats');
+$category_data = $this->model_catalog_tree_cats->getCategory($category_id);
+
+
+
+
+					$data['products'][] = array(
+						'product_id'  => $product_info['product_id'],
+						'thumb'       => $image,
+						'name'        => $product_info['name'],
+						'description' => utf8_substr(strip_tags(html_entity_decode($product_info['description'], ENT_QUOTES, 'UTF-8')), 0, $this->config->get('theme_' . $this->config->get('config_theme') . '_product_description_length')) . '..',
+						'price'       => $price,
+						'special'     => $special,
+						'tax'         => $tax,
+						'rating'      => $rating,
+						'href'        => $this->url->link('product/product', 'product_id=' . $product_info['product_id']),
+						'id_category' => $category_data["category_id"],
+						'name_category' => $category_data["name"]
+					);
+				}
+			}
+		}
+
+		if ($data['products']) {
+			return $this->load->view('extension/module/featured', $data);
+		}
+	}
+
+}
---
+++ catalog/controller/extension/module/tree_cats.php
@@ -0,0 +1,60 @@
+<?php
+class ControllerExtensionModuleTreeCats extends Controller {
+	public function index() {
+		$this->load->language('extension/module/tree_cats');
+    $data['test_module'] = 'modele test!!!';
+
+		if (isset($this->request->get['path'])) {
+			$parts = explode('_', (string)$this->request->get['path']);
+		} else {
+			$parts = array();
+		}
+
+		if (!empty($parts)) {
+			$data['active'] = end($parts);
+		} else {
+			$data['active'] = 0;
+		}
+
+		$this->load->model('catalog/tree_cats');
+
+		$categories = $this->model_catalog_tree_cats->getTreeCats();
+
+		foreach($categories as $id => $category){
+			$categories[$id]['href'] = $this->url->link('product/category', 'path=' . $id);
+		}
+
+		$category_tree = $this->model_catalog_tree_cats->getMapTree($categories);
+
+		$data["categories_tree"] = $category_tree;
+
+		$subcategories = [];
+		$grandson = [];
+		foreach($category_tree as $id => $category_item){
+			if(isset($category_item["childs"])){
+				$subcategories[$id] = $category_item["childs"];
+				foreach($category_item["childs"] as $sub_id => $item_subchild){
+					if(isset($item_subchild["childs"])){
+						$grandson[$sub_id] = $item_subchild["childs"];
+					}
+				}
+			}
+		}
+
+		$quant = $this->cart->countProducts();
+		$data['text_items_quantity'] = $quant;
+
+		$data['subcategories'] = $subcategories;
+		$data['grandson_cat'] = $grandson;
+
+		$data['home'] = $this->url->link('common/home');
+		$data['contact'] = $this->url->link('information/contact');
+		$data['shopping_cart'] = $this->url->link('checkout/cart');
+
+		return $this->load->view('extension/module/cats_template', $data);
+
+
+	}
+
+
+}
