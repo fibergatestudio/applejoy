372aa7341eb813e1c86bda1620c9896f0fa64ea2
NOGlory <imlagoyda@gmail.com>
2021-08-26 09:21:58 +0300

    add repair

---
+++ admin/model/extension/module/repair.php
@@ -0,0 +1,56 @@
+<?php
+class ModelExtensionModuleRepair extends Model {
+	
+	public function getTotalOrders() {
+      	$query = $this->db->query("SELECT COUNT(*) AS total FROM " . DB_PREFIX . "repair_order");
+		
+		return $query->row['total'];
+	}	
+
+	public function createDatabaseTable() {
+		$sql  = "CREATE TABLE IF NOT EXISTS `".DB_PREFIX."repair_order` ( ";
+		$sql .= "`id` int(11) NOT NULL AUTO_INCREMENT, ";
+		$sql .= "`name` varchar(96) NOT NULL, ";
+		$sql .= "`phone` varchar(10) NOT NULL, ";
+		$sql .= "`product` varchar(96) DEFAULT NULL, ";
+		$sql .= "`message` text  DEFAULT NULL, ";
+		$sql .= "`date` DATE NOT NULL DEFAULT CURRENT_TIMESTAMP, ";
+		$sql .= "PRIMARY KEY (`id`) ";
+		$sql .= ") ENGINE=MyISAM";
+		$this->db->query($sql);
+	}
+	
+	
+	public function dropDatabaseTable() {
+		$sql = "DROP TABLE IF EXISTS `".DB_PREFIX."repair_order`;";
+		$this->db->query($sql);
+	}
+
+	public function createRepairOrder()
+	{			
+		$res0 = $this->db->query("SHOW TABLES LIKE '". DB_PREFIX ."repair_order'");
+		if($res0->num_rows == 0){
+			$sql  = "CREATE TABLE IF NOT EXISTS `".DB_PREFIX."repair_order` ( ";
+			$sql .= "`id` int(11) NOT NULL AUTO_INCREMENT, ";
+			$sql .= "`name` varchar(96) NOT NULL, ";
+			$sql .= "`phone` varchar(10) NOT NULL, ";
+			$sql .= "`product` varchar(96) DEFAULT NULL, ";
+			$sql .= "`message` text  DEFAULT NULL, ";
+			$sql .= "`date` DATE NOT NULL DEFAULT CURRENT_TIMESTAMP, ";
+			$sql .= "PRIMARY KEY (`id`) ";
+			$sql .= ") ENGINE=MyISAM";
+			$this->db->query($sql);
+		}	
+	}
+	
+	public function getRepairOrder() {
+		$query = $this->db->query("SELECT * FROM ". DB_PREFIX ."repair_order"); 
+
+		return $query->rows;
+	}
+
+	public function deleteRepairOrder($id) {
+		$query = $this->db->query("DELETE FROM ". DB_PREFIX ."repair_order WHERE id=".$id); 
+	}
+
+}
---
+++ catalog/controller/repair/repair.php
@@ -0,0 +1,40 @@
+<?php
+class ControllerRepairRepair extends Controller {
+	private $error = array();
+
+	public function index() {
+		$this->load->language('repair/repair');
+		$this->load->model('repair/repair');
+
+		$this->document->setTitle($this->language->get('heading_title'));
+
+		$data['breadcrumbs'] = array();
+
+		$data['breadcrumbs'][] = array(
+			'text' => $this->language->get('text_home'),
+			'href' => $this->url->link('common/home')
+		);
+
+		$data['breadcrumbs'][] = array(
+			'text' => $this->language->get('heading_title'),
+			'href' => $this->url->link('repair/repair')
+		);
+
+		$data['column_left'] = $this->load->controller('common/column_left');
+		$data['column_right'] = $this->load->controller('common/column_right');
+		$data['content_top'] = $this->load->controller('common/content_top');
+		$data['content_bottom'] = $this->load->controller('common/content_bottom');
+		
+		$data['faq'] = $this->load->controller('repair/faq');
+		
+		$data['modal'] = $this->load->controller('extension/module/repair');
+
+		$data['footer'] = $this->load->controller('common/footer');
+		$data['header'] = $this->load->controller('common/header');
+
+		$data['categories'] = $this->model_repair_repair->getCategoriesMock();
+
+		$this->response->setOutput($this->load->view('repair/repair', $data));
+	}
+
+}
