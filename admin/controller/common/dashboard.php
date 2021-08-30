e9d29af06a5be0b456cb840db25c4e5d185f169e
NOGlory <imlagoyda@gmail.com>
2021-08-30 14:42:01 +0300

    admin menu fix

--- admin/controller/common/dashboard.php
+++ admin/controller/common/dashboard.php
@@ -87,7 +87,7 @@ class ControllerCommonDashboard extends Controller {
 		}
 
 		$data['header'] = $this->load->controller('common/header');
-		$data['column_left'] = $this->load->controller('common/column_menu');
+		$data['column_left'] = $this->load->controller('common/column_left');
 		$data['footer'] = $this->load->controller('common/footer');
 
 		// Run currency update
