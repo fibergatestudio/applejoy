bd3822ae25c1a5e160d2d6fe0d9940f142d104e5
LeonSoftwares <LeonSoft.mail@mail.ru>
2021-10-22 20:33:45 +0300

    Update OCFilter to 4.8.0.16
    Update OCFilter to 4.8.0.16

--- catalog/controller/extension/feed/ocfilter_sitemap.php
+++ catalog/controller/extension/feed/ocfilter_sitemap.php
@@ -1,38 +1,102 @@
-<?php
-class ControllerExtensionFeedOcfilterSitemap extends Controller {
-	public function index() {
-		//if ($this->config->get('ocfilter_sitemap_status')) {
-			$output  = '<?xml version="1.0" encoding="UTF-8"?>';
-			$output .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">';
-
-      $this->load->model('extension/module/ocfilter');
-
-			$ocfilter_pages = $this->model_extension_module_ocfilter->getPages();
-
-			foreach ($ocfilter_pages as $page) {
-        $link = rtrim($this->url->link('product/category', 'path=' . $page['category_id']), '/');
-
-        if ($page['keyword']) {
-        	$link .= '/' . $page['keyword'];
-        } else {
-        	$link .= '/' . $page['params'];
-        }
-
-        if ($this->config->get('config_seo_url_type') == 'seo_pro') {
-        	$link .= '/';
-        }
-
-				$output .= '<url>';
-				$output .= '<loc>' . $link . '</loc>';
-				$output .= '<changefreq>weekly</changefreq>';
-				$output .= '<priority>0.7</priority>';
-				$output .= '</url>';
-			}
-
-			$output .= '</urlset>';
-
-			$this->response->addHeader('Content-Type: application/xml');
-			$this->response->setOutput($output);
-		//}
-	}
-}
+<?php
+class ControllerExtensionFeedOCFilterSitemap extends Controller {
+  private $category_link = [];
+  private $seo_url = false;
+  private $url_suffix = null;
+   
+	public function index() {   
+    if (!$this->registry->has('ocfilter') || !$this->ocfilter->config('sitemap_status')) {
+      return;
+    }    
+    
+    $this->seo_url = $this->ocfilter->seo->isSeoUrlEnabled();   
+        
+    $language_query = $this->db->query("SELECT * FROM " . DB_PREFIX . "language WHERE status = '1'");
+
+    $multi_language = ($language_query->num_rows > 1);
+      
+    if ($multi_language) {
+      if ($this->ocfilter->opencart->version >= 30) {
+        $seo_url_query = $this->db->query("SELECT `query` FROM " . DB_PREFIX . "seo_url WHERE `query` LIKE 'category_id=%' LIMIT 1");
+      } else {
+        $seo_url_query = $this->db->query("SELECT `query` FROM " . DB_PREFIX . "url_alias WHERE `query` LIKE 'category_id=%' LIMIT 1");
+      }
+      
+      if ($seo_url_query->num_rows) {
+        $compare_links = [];
+        
+        foreach ($language_query->rows as $language) {
+          $this->config->set('config_language', $language['code']);	
+          $this->config->set('config_language_id', $language['language_id']);
+          
+          $compare_links[] = $this->url->link('product/category', 'path=' . str_replace('category_id=', '', $seo_url_query->row['query']));  
+        }
+
+        $multi_language = (count(array_unique($compare_links)) > 1);        
+      } else {
+        $multi_language = false;
+      }
+    }
+    
+    // Output
+    $output  = '<?xml version="1.0" encoding="UTF-8"?>';
+    $output .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">';
+         
+    if (!$multi_language) {
+      $output .= $this->getLanguageOutput(0);
+    } else {
+      foreach ($language_query->rows as $language) {
+        $this->config->set('config_language', $language['code']);	
+        $this->config->set('config_language_id', $language['language_id']);	
+        
+        $output .= $this->getLanguageOutput($language['language_id']);
+      } 
+    }
+
+    $output .= '</urlset>';
+
+    $this->ocfilter->opencart->responseXML($output);
+	}
+  
+  protected function getLanguageOutput($language_id) {
+    $output = '';
+    
+    $pages = $this->model_extension_module_ocfilter->getPages([ 'filter_sitemap' => true, ]);
+    
+    foreach ($pages as $page) {
+      $cache_key = $language_id . '.' . $page['category_id'];
+      
+      if (isset($this->category_link[$cache_key])) {
+        $link = $this->category_link[$cache_key];
+      } else {
+        $link = $this->url->link('product/category', 'path=' . $page['path'], 'SSL');  
+        
+        if (is_null($this->url_suffix)) {
+          $this->url_suffix = $this->ocfilter->seo->getUrlSuffix($link);
+        }
+      
+        $link = rtrim($link, '/');  
+        
+        if (false !== utf8_strpos($link, '.html')) {
+          $link = utf8_substr($link, 0, utf8_strpos($link, '.html'));
+        }  
+        
+        $this->category_link[$cache_key] = $link;
+      }
+        
+      if ($page['keyword'] && $this->seo_url && false === strpos($link, 'index.php?route=')) {
+        $link .= '/' . $page['keyword'] . $this->url_suffix;
+      } else {
+        $link .= (false === utf8_strpos($link, '?') ? '?' : '&amp;') . $this->ocfilter->params->getIndex() . '=' . $this->ocfilter->params->encode(json_decode($page['params'], true));
+      }           
+      
+      $output .= '<url>';
+      $output .= '<loc>' . $link . '</loc>';
+      $output .= '<changefreq>weekly</changefreq>';
+      $output .= '<priority>0.6</priority>';
+      $output .= '</url>';
+    }
+    
+    return $output;
+  }
+}
