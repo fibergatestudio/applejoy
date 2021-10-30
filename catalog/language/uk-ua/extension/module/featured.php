c12e95381605f78b577a7a7cea0f60a72a753bce
LeonSoftwares <LeonSoft.mail@mail.ru>
2021-10-30 13:20:25 +0300

    Fix featured products
    Fix featured products

--- catalog/language/uk-ua/extension/module/featured.php
+++ catalog/language/uk-ua/extension/module/featured.php
@@ -1,6 +1,20 @@
-<?php
-// Heading
-$_['heading_title'] = 'Рекомендовані';
-
-// Text
+<?php
+
+// Heading
+
+$_['heading_title'] = 'Рекомендовані';
+
+
+
+// Text
+
 $_['text_tax'] = 'Без ПДВ:';
+
+
+$_['product_price'] = 'Цiна';
+
+
+$_['product_buy'] = 'Купити';
+
+
+$_['product_buy_one_click'] = 'Купити в 1 клiк';
