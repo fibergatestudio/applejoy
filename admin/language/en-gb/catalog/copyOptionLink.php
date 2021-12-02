98a55e9c19b4ba3a390b3fd37b2a19315597201d
LeonSoftwares <LeonSoft.mail@mail.ru>
2021-12-02 21:11:52 +0300

    Module product option Link
    Module product option Link

---
+++ admin/language/en-gb/catalog/copyOptionLink.php
@@ -0,0 +1,39 @@
+<?php
+
+// Heading
+$_['heading_title']    			= 'Check the values of the options you create';
+
+// Text
+$_['text_on']         			= 'On';
+$_['text_off']        			= 'Off';
+$_['text_new']         			= '--Create New--';
+$_['text_join']         		= 'Add to existing';
+$_['text_copyOptionLink_no']    = 'No data to display';
+$_['text_option']        		= 'Option';
+$_['text_product']        	    = 'Product';
+$_['link_view_0']      			= 'List';
+$_['link_view_1']      			= 'Table';
+$_['link_view_2']      			= 'SELECT drop-down list';
+$_['link_view_3']      			= 'Extended table';
+$_['link_danger']      			= 'ATTENTION! The current option belongs to the "Product Link" type. When you click the "Save" button, all the saved data for the current product will be deleted <br> <br> To display and save the settings, set the "Product Option Link" option to the enabled status';
+
+
+// Entry
+$_['entry_option_value']  		= 'Option Value';
+$_['entry_close']     			= 'Close';
+$_['entry_save']    			= 'Apply Changes';
+$_['entry_link_view']      		= 'Display option:';
+$_['entry_link_name']      		= 'Name';
+$_['entry_link_image']      	= 'Image';
+$_['entry_copy_name']      	    = 'Main product';
+$_['entry_copy_name_option']    = 'New link to product';
+$_['entry_product_option']  	= 'Option:';
+$_['entry_copy_data']  			= 'The data was successfully saved';
+$_['entry_copy_data_error']  	= 'An error occurred while saving';
+$_['entry_product_option_danger']  	= 'The item already has this option. <br>Check the add algorithm';
+$_['entry_product_link_danger']  	= 'Option with this item already exists.';
+$_['entry_link']      			= 'Select a new product:';
+$_['entry_linkproduct']      	= 'Current position:';			
+$_['entry_link_copy']      	    = 'Copy Option';
+$_['entry_link_copy_title']     = 'Copy the option to the linked products';
+$_['entry_modal_product_main']  = 'Specify the option value for the current product';
---
+++ admin/language/en-gb/extension/module/ProductOptionLink.php
@@ -0,0 +1,54 @@
+<?php
+// *	ProductOptionLink
+
+// Heading
+$_['heading_title']  	  = 'Product Option Link - Option as a link to an item';
+
+// Табы
+$_['tab_settings_admin']    	  = 'Settings Admin parts';
+$_['tab_settings_user']    	  	  = 'User Part Settings';
+$_['tab_css']    		 		  = 'Button styles';
+
+// Text
+$_['text_module']      = 'Modules';
+$_['text_success']     = 'Settings changed successfully!';
+$_['text_edit']        = 'Editing';
+$_['text_on']          = 'On';
+$_['text_off']         = 'Off';
+$_['text_option']         = 'Photo Options';
+$_['text_product']         = 'Product Photos';
+$_['link_view_0']      			= 'List';
+$_['link_view_1']      			= 'Table';
+$_['link_view_2']      			= 'SELECT drop-down list';
+$_['link_view_3']      			= 'Extended table';
+
+
+$_['text_info_tab_settings_admin']      	 = 'Module settings for the administrative part of the store';
+$_['text_info_tab_settings_user']      	 = 'Settings of the user part of the store';
+$_['text_info_tab_css']        		 = 'Prescribe the styles of the output buttons';
+$_['text_info_tab_css_mini']         = 'You must specify styles with all the rules for writing CSS without specifying the class names. <br> You can see an example of writing in the Stylesheet.css file';
+
+
+// Entry
+$_['entry_status']     			= 'Status';
+$_['entry_style_name']     		= 'Option name:';
+$_['entry_style_link']     		= 'Basic link to another product:';
+$_['entry_style_product']     	= 'If the link is the same as the current item:';
+$_['entry_style_noproduct']     = 'If the item is not specified:';
+$_['entry_on_of_admin']         = 'Set default values ​​(these parameters can be changed directly in the product card when adding an option)';
+$_['entry_on_off_name']         = 'Output NAMES options';
+$_['entry_on_off_images']       = 'PICTURE options output';
+$_['entry_on_off_fullcheck']    = 'Enable full scan when copying';
+$_['entry_on_off_save']    		= 'Batch save when copying';
+$_['entry_link_view']           = 'Option display options';
+$_['entry_on_off_stock']        = 'Do not show the option if there is no stock in the warehouse (quantity = "0")';
+$_['entry_resize']        		= 'Image size (default 50х50)';
+$_['entry_resize_length']       = 'Length:';
+$_['entry_resize_height']       = 'Height:';
+
+// Help
+$_['help_fullcheck']       		= 'When copying options, every created option will be checked with the goods already in place to avoid duplication (by default, only the presence of the same option in the main product is checked). <br> Increased server load!';
+$_['help_save']       			= 'When copying data will be sent in batches. Turn on this option when a large number of connections are created at the same time and there are not enough resources, which leads to data loss. <br>The server load will increase!';
+
+// Error
+$_['error_permission'] = 'You do not have sufficient permissions to make changes!';
